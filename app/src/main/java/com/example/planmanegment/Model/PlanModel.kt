a9d1bb7ba9bc410d3b83de76ae95f874138e9676
Swarnajith <IT21253676@my.sliit.lk>
2023-05-06 20:57:37 +0530

    added moneymngement

---
+++ app/src/main/java/com/example/planmanegment/Model/PlanModel.kt
@@ -0,0 +1,11 @@
+package com.example.planmanegment.Model
+
+data class PlanModel(
+
+var planId:String?=null,
+var PlanDescription:String?=null,
+var planAmount:String?=null,
+var planCategory:String?=null,
+var LoginEmail:String?=null,
+
+)
